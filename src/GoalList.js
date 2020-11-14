3a04d7c7b67b710100f3ec0af291e1e07946b848
Matt Lane <lane.matthew@gmail.com>
2020-11-13 16:30:56 -0800

    add total to GoalList

--- src/GoalList.js
+++ src/GoalList.js
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import NewGoalForm from "./NewGoalForm";
 import Goal from "./Goal";
+import "./GoalList.css";
+
+function calculateGoalTotal(goals) {
+  let total = 0;
+  for (let goal of goals) {
+    total += goal.count;
+  }
+  return total;
+}
 
 function GoalList(props) {
   const [goals, setGoals] = useState([
     { description: "Exercise", count: 3 },
     { description: "No sweets", count: 5 }
   ]);
+
   return (
     <div>
       <h1>Weekly Goals!</h1>
       <NewGoalForm />
       {goals.map(goal => <Goal {...goal} />)}
+      <h4 className="GoalList-total">Total number of weekly goals: {calculateGoalTotal(goals)}</h4>
     </div>
   );
 }
