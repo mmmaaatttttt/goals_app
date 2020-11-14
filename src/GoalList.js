1ad5a261afad7373381f1fc04dd0b6eb5a41b246
Matt Lane <lane.matthew@gmail.com>
2020-11-13 16:56:06 -0800

    fix bug in calculateGoalTotal

--- src/GoalList.js
+++ src/GoalList.js
@@ -6,23 +6,28 @@ import "./GoalList.css";
 function calculateGoalTotal(goals) {
   let total = 0;
   for (let goal of goals) {
-    total += goal.count;
+    total += +goal.count;
   }
   return total;
 }
 
 function GoalList(props) {
-  const [goals, setGoals] = useState([
-    { description: "Exercise", count: 3 },
-    { description: "No sweets", count: 5 }
-  ]);
+  const [goals, setGoals] = useState([]);
+
+  function add(newGoal) {
+    setGoals(oldGoals => [newGoal, ...oldGoals]);
+  }
 
   return (
     <div>
       <h1>Weekly Goals!</h1>
-      <NewGoalForm />
-      {goals.map(goal => <Goal {...goal} />)}
-      <h4 className="GoalList-total">Total number of weekly goals: {calculateGoalTotal(goals)}</h4>
+      <NewGoalForm add={add} />
+      {goals.map(goal => (
+        <Goal {...goal} />
+      ))}
+      <h4 className="GoalList-total">
+        Total number of weekly goals: {calculateGoalTotal(goals)}
+      </h4>
     </div>
   );
 }
