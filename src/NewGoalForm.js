f3a88abcfb1c6bbd802c33e89e0bfae9ae3f379d
Matt Lane <lane.matthew@gmail.com>
2020-11-13 16:47:03 -0800

    adding controlled NewGoalForm component

--- src/NewGoalForm.js
+++ src/NewGoalForm.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import "./NewGoalForm.css";
 
 function NewGoalForm(props) {
+  const [formData, setFormData] = useState({
+    description: "",
+    count: 1
+  });
+
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setFormData(oldData => ({ ...oldData, [name]: value }));
+  }
+
   return (
-    <div>NewGoalForm</div>
-  )
+    <form className="NewGoalForm">
+      <div className="NewGoalForm-input">
+        <label>What is your goal?</label>
+        <input
+          placeholder="Enter your goal here"
+          onChange={handleChange}
+          value={formData.description}
+          name="description"
+        />
+      </div>
+      <div className="NewGoalForm-input">
+        <label>How many days per week do you want to meet this goal?</label>
+        <input
+          type="number"
+          min="1"
+          max="7"
+          step="1"
+          onChange={handleChange}
+          value={formData.count}
+          name="count"
+        />
+      </div>
+      <input
+        type="submit"
+        value="Add this goal!"
+        className="NewGoalForm-submit"
+      />
+    </form>
+  );
 }
 
 export default NewGoalForm;
